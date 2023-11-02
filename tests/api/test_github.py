169169070c5d3e6baae3a0f0647be324e32b90ee
A-d-a-m <adam3928@mail.ru>
2023-11-02 09:50:50 +0200

    added headers

--- tests/api/test_github.py
+++ tests/api/test_github.py
@@ -12,6 +12,11 @@ def github_api(method, url, **kwargs):
     base_url = 'https://api.github.com'
     new_url = base_url + url
     method = method.upper()
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Accept': 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
     with allure.step(f'Отправляем запрос {method} {url} {args if len(args) != 0 else ""} '):
         with sessions.Session() as session:
             response = session.request(method=method, url=new_url, **kwargs)
