2bef675b7667a96150931e371b6124236c969866
A-d-a-m <adam3928@mail.ru>
2023-11-15 23:46:54 +0200

    replaced expected_schema and response in validate_response_json

--- qa_guru_diploma/utils/response_utils.py
+++ qa_guru_diploma/utils/response_utils.py
@@ -4,14 +4,14 @@ from allure_commons.types import AttachmentType
 import allure
 
 
-def check_status_code(response_status_code, expected_status_code):
+def check_status_code(expected_status_code, response_status_code):
     with allure.step(f'Проверяем, что пришел status code = {expected_status_code}'):
         assert response_status_code == expected_status_code, (f'Wrong response status code: '
                                                               f'expected status code = {expected_status_code}, '
                                                               f'got {response_status_code}')
 
 
-def validate_response_json(response, expected_schema):
+def validate_response_json(expected_schema, response):
     with allure.step('Проверяем содержание response.json'):
         try:
             jsonschema.validate(response.json(), expected_schema)
