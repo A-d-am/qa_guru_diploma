b92292b4e884c941c71a34c54e7be01b5ec021c6
A-d-a-m <adam3928@mail.ru>
2023-11-15 23:41:10 +0200

    reworked

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,62 +1,62 @@
 from selenium import webdriver as selenium_webdriver
 from appium import webdriver as appium_webdriver
-from selene import browser
+from selene import browser, support
 import pytest
 import allure
-from qa_guru_diploma.data.user import User
-from qa_guru_diploma.data import user_data
+import allure_commons
 from qa_guru_diploma import utils
 from qa_guru_diploma.utils import allure_utils
 import project
-
-
-@pytest.fixture(scope='function')
-def user():
-    user = User(
-        full_name=user_data.user_full_name,
-        email=user_data.user_login_email,
-        phone=user_data.user_login_phone,
-        password=user_data.user_password
-    )
-    return user
-
 
 project_config = project.Config(_env_file=utils.file.relative_from_root(f'.env.{project.Config().context}'))
 
 
 @pytest.fixture(scope='function', autouse=True)
 def driver_management(request):
-    with allure.step('init session'):
-        if request.param == 'web' or 'api':
-            browser.config.base_url = project_config.base_url
-            browser.config.driver_options = project_config.driver_options(request.param)
-            browser.config.window_height = 1080
-            browser.config.window_width = 1920
-
-        elif request.param == 'ios' or 'android':
+    if request.param in ['ios', 'android']:
+        with allure.step('init app session'):
             browser.config.driver = appium_webdriver.Remote(
                 project_config.driver_remote_url, options=project_config.driver_options(request.param)
             )
 
+        browser.config.timeout = project_config.timeout
+        browser.config._wait_decorator = support._logging.wait_with(
+            context=allure_commons._allure.StepContext
+        )
+
+    elif request.param == 'web':
+        browser.config.base_url = project_config.base_url
+        browser.config.window_height = 1080
+        browser.config.window_width = 1920
+
+        driver = selenium_webdriver.Remote(
+            command_executor=project_config.get_selenoid_link(),
+            options=project_config.driver_options(request.param)
+        )
+        browser.config.driver = driver
+
+    elif request.param == 'api':
+        pass
+
     yield
 
-    if project_config.context == 'web':
+    if request.param == 'web':
         allure_utils.attach_html(browser)
         allure_utils.attach_web_logs(browser)
         allure_utils.attach_video(browser)
-    #TODO: добавить api аттачи
-    allure_utils.attach_screenshot(browser)
-
-    session_id = browser.driver.session_id
+        allure_utils.attach_screenshot(browser)
 
     if project_config.context in ['bstack', 'local_emulator', 'local_real']:
         allure_utils.attach_mobile_page_source(browser)
 
+    session_id = browser.driver.session_id
+
     with allure.step('tear down app session with id: ' + session_id):
         browser.quit()
 
-    if project_config.is_bstack_run(request.param):
-        allure_utils.attach_bstack_video(session_id, project_config.bstack_userName, project_config.bstack_accessKey)
+    if project_config.context == 'bstack':
+        allure_utils.attach_bstack_video(session_id, project_config.bstack_userName,
+                                         project_config.bstack_accessKey)
 
 
 ios = pytest.mark.parametrize('driver_management', ['ios'], indirect=True)
